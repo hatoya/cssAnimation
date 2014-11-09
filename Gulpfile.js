67a40dd5abd3e60e084764f7a60009502f1cdd6c
WadaHayato <hatoya.web.design@gmail.com>
2014-11-09 18:08:43 +0900

    first push

---
+++ Gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+	connect = require('gulp-connect'),
+	sass = require('gulp-ruby-sass'),
+	ts = require('gulp-typescript');
+
+gulp.task('connect', function(){
+	connect.server({
+		root: './app',
+		port: 7777,
+		livereload: true
+	});
+});
+
+gulp.task('sass', function(){
+	gulp
+		.src('./scss/style.scss')
+		.pipe(sass({style: 'expanded'}))
+		.pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('ts', function(){
+	gulp
+		.src('./ts/*.ts')
+		.pipe(gulp.dest('./app/js'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('./scss/style.scss', ['sass']);
+	gulp.watch('./ts/*.ts', ['ts']);
+	gulp.watch(['./app/*.html', './app/css/style.css', './app/js/*.js'], function(){
+		gulp.src('app/*.html').pipe(connect.reload());
+	});
+});
+
+gulp.task('default', ['connect', 'watch']);
